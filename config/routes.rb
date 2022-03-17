6f147953c6c2ca9ec48b7ca17eb09acbbde16a8b
Bruno Pena <brunoph26@gmail.com>
2022-03-16 23:21:26 -0500

    Creating order_items model and controller, modifying the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
+  get 'carts/show'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  delete 'carts/:id', to: "carts#destroy"
+
+  post 'order_items/:id/add', to:  "order_items#add_quantity", as: "order_item_add"
+  post 'order_items/:id/reduce' => "order_items#reduce_quantity", as: "order_item_reduce"
+  post 'order_items' => "order_items#create"
+  get 'order_items/:id' => "order_items#show", as: "order_item"
+  delete 'order_items/:id' => "order_items#destroy"
 
   resources :products
+  resources :orders
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_17_034354) do
+ActiveRecord::Schema.define(version: 2022_03_17_040738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,17 @@ ActiveRecord::Schema.define(version: 2022_03_17_034354) do
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.integer "order_id"
+    t.integer "quantity", default: 1
+    t.bigint "product_id"
+    t.bigint "cart_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["cart_id"], name: "index_order_items_on_cart_id"
+    t.index ["product_id"], name: "index_order_items_on_product_id"
   end
 
   create_table "orders", force: :cascade do |t|
