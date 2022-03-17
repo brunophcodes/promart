e3d3f9fd8a6fd65d7de6848d19cb37943c86d9b0
Bruno Pena <brunoph26@gmail.com>
2022-03-16 22:19:09 -0500

    creating cart model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_17_011239) do
+ActiveRecord::Schema.define(version: 2022_03_17_030642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
